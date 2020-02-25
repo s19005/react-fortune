f0cf901faa8ed56f05d61bf832839dbcc164fec7
s19005 <s19005@std.it-college.ac.jp>
2020-02-25 07:18:43 +0000

    Updates

---
+++ precache-manifest.e7d559b9d51e24cc5b12168402c1a1b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f07396b751357b69b8f4cd0b1882b4",
+    "url": "/react-fortune/index.html"
+  },
+  {
+    "revision": "6a45aeb2c63f3be411d0",
+    "url": "/react-fortune/static/css/main.7f205a02.chunk.css"
+  },
+  {
+    "revision": "e4936e68fa60e65072ce",
+    "url": "/react-fortune/static/js/2.9c411de3.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/react-fortune/static/js/2.9c411de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a45aeb2c63f3be411d0",
+    "url": "/react-fortune/static/js/main.c49a6499.chunk.js"
+  },
+  {
+    "revision": "dad638424633a8ae571f",
+    "url": "/react-fortune/static/js/runtime-main.e0222ae9.js"
+  }
+]);
